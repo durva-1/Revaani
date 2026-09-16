04fbdb54a10237bd03c1a2fd329c00e4d5c7999a
durva-1 <dvyas394@gmail.com>
2026-09-16 12:05:13 +0530

    Update TTS.py

--- backend/TTS.py
+++ backend/TTS.py
@@ -1,1 +1,9 @@
+class TextToSpeech:
 
+    def __init__(self):
+        self.language = "Gujarati"
+
+    def convert_to_speech(self, text):
+        print("Converting text to Gujarati speech...")
+        audio_file = "response_audio.mp3"
+        return audio_file
