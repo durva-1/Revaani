94d4f0729d6ca4a9e741b5df1d189870605c855f
durva-1 <dvyas394@gmail.com>
2026-09-16 12:03:06 +0530

    Update STT.py

--- backend/STT.py
+++ backend/STT.py
@@ -1,1 +1,10 @@
+def __init__(self):
+        self.language = "Gujarati"
 
+    def transcribe(self, audio_file):
+        print("Processing audio:", audio_file)
+        text = "નમસ્તે, તમે કેમ છો?"
+        return text
+
+    def get_language(self):
+        return self.language
