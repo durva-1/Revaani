07814b6720d9ea9437f37dd9c3ed9e1590c8d3f2
durva-1 <dvyas394@gmail.com>
2026-09-16 12:01:29 +0530

    Update user.py

--- backend/user.py
+++ backend/user.py
@@ -1,1 +1,25 @@
+def __init__(self, email, full_name, region, age_group, gender="Not specified"):
+        self.email = email
+        self.full_name = full_name
+        self.region = region
+        self.age_group = age_group
+        self.gender = gender
+        self.points = 0
+        self.is_admin = False
 
+    def update_profile(self, full_name, region, age_group, gender):
+        self.full_name = full_name
+        self.region = region
+        self.age_group = age_group
+        self.gender = gender
+
+    def add_points(self, points):
+        self.points += points
+
+    def display_user(self):
+        print("Name:", self.full_name)
+        print("Email:", self.email)
+        print("Region:", self.region)
+        print("Age Group:", self.age_group)
+        print("Gender:", self.gender)
+        print("Points:", self.points)
