6afd23220b302c5a5fcff994e49b32c06b67eeab
durva-1 <dvyas394@gmail.com>
2026-09-16 12:03:49 +0530

    Update DialectDetector.py

--- backend/DialectDetector.py
+++ backend/DialectDetector.py
@@ -1,1 +1,14 @@
+def __init__(self):
+        self.accent = "Unknown"
 
+    def detect_accent(self, region):
+        if region == "Ahmedabad":
+            self.accent = "Ahmedabad Gujarati"
+        elif region == "Surat":
+            self.accent = "Surti Gujarati"
+        elif region == "Saurashtra":
+            self.accent = "Saurashtrian Gujarati"
+        else:
+            self.accent = "Gujarati"
+
+        return self.accent
